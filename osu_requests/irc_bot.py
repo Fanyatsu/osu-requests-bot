a7f839e5a3b4976052686ecc700a68c21aa4082e
Vitaly Tkachov <39880934+Fanyatsu@users.noreply.github.com>
2023-06-27 19:12:10 +0300

    Make IrcBot initialization using super()

--- osu_requests/irc_bot.py
+++ osu_requests/irc_bot.py
@@ -7,7 +7,7 @@ from irc.client import ServerConnection, Event
 class IrcBot(SingleServerIRCBot):
     def __init__(self, username, server, port=6667, password=None):
         recon = ExponentialBackoff(min_interval=5, max_interval=30)
-        SingleServerIRCBot.__init__(self, [(server, port, password)], username, username, recon=recon)
+        super().__init__(server_list=[(server, port, password)], nickname=username, realname=username, recon=recon)
         self.messages_queue = asyncio.Queue()
 
     def on_welcome(self, c: ServerConnection, e: Event):
